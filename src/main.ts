d405fe3cf2972e91291db0883a920df92a098250
accela673 <101983496+accela673@users.noreply.github.com>
2022-11-25 17:25:57 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Car sharing')
+    .setTitle('Blog service')
     .setDescription(`Simple blog-service`)
     .setVersion('1.0')
     .addBearerAuth()
